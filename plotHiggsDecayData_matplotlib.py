81c9cbeb91b7d5e4ab8657e24c97e66b556dd1f1
Simone Bologna <smnbologna@gmail.com>
2017-04-12 17:30:50 +0100

    Added some comments and improved plot names

--- plotHiggsDecayData_matplotlib.py
+++ plotHiggsDecayData_matplotlib.py
@@ -18,6 +18,8 @@ higgsDecaysTree = higgsFile.Get("higgsDecays")
 
 numberOfEntries = higgsDecaysTree.GetEntries()
 
+# ---- Preparing data container structures
+
 ''' Array with the pt distribution of the Higgs bosons'''
 higgsPtData = []
 ''' Array with the Cumulative pt distribution of both EW bosons'''
@@ -56,6 +58,8 @@ higgsPhiData = []
 ewBosonPhiData = []
 ''' Array with the Cumulative phi distribution of both charged leptons'''
 chargedLeptonPhiData = []
+
+# ---- Retrieving event data ----
 
 for eventIndex in xrange(0, numberOfEntries):
   higgsDecaysTree.GetEntry(eventIndex)
@@ -143,6 +147,9 @@ for eventIndex in xrange(0, numberOfEntries):
   if isLepton3Charged : chargedLeptonPhiData.append(lepton3Phi)
   if isLepton4Charged : chargedLeptonPhiData.append(lepton4Phi)
 
+
+# ---- Creating canvases (window), and plot objects ----
+
 higgsPtCanvas = plt.figure()
 ''' Histogram with the pt distribution of the Higgs bosons'''
 higgsPtPlot = higgsPtCanvas.add_subplot(1, 1, 1)
@@ -185,6 +192,9 @@ chargedLeptonPhiCanvas = plt.figure()
 ''' Histogram with the Cumulative phi distribution of both charged leptons'''
 chargedLeptonPhiPlot = chargedLeptonPhiCanvas.add_subplot(1, 1, 1)
 
+
+# ---- Building up plots ----
+
 higgsPtPlot.hist(higgsPtData, bins=100, histtype="step")
 ewBosonPtPlot.hist(ewBosonPtData, bins=100, histtype="step")
 ewBosonLeadingPtPlot.hist(ewBosonLeadingPtData, bins=100, histtype="step") 
@@ -214,100 +224,102 @@ higgsPhiPlot.hist(higgsPhiData, bins=100, histtype="step")
 ewBosonPhiPlot.hist(ewBosonPhiData, bins=100, histtype="step")
 chargedLeptonPhiPlot.hist(chargedLeptonPhiData, bins=100, histtype="step")
 
+# ---- Setting up, drawing, and saving plots ----
+
 higgsPtPlot.set_xlabel("pt [GeV]")
 higgsPtPlot.set_ylabel("Counts")
 higgsPtPlot.set_title("Higgs boson pt distribution")
 higgsPtPlot.grid(True)
 higgsPtPlot.legend(loc="upper right")
-higgsPtCanvas.savefig("higgsPtPlot.svg")
+higgsPtCanvas.savefig(argv[0] + "_higgsPtPlot.svg")
 
 ewBosonPtPlot.set_xlabel("pt [GeV]")
 ewBosonPtPlot.set_ylabel("Counts")
 ewBosonPtPlot.set_title("EW bosons pt distribution")
 ewBosonPtPlot.grid(True)
 ewBosonPtPlot.legend(loc="upper right")
-ewBosonPtCanvas.savefig("ewBosonPtPlot.svg")
+ewBosonPtCanvas.savefig(argv[0] + "_ewBosonPtPlot.svg")
 
 ewBosonLeadingPtPlot.set_xlabel("pt [GeV]")
 ewBosonLeadingPtPlot.set_ylabel("Counts")
 ewBosonLeadingPtPlot.set_title("EW boson leading pt distribution")
 ewBosonLeadingPtPlot.grid(True)
 ewBosonLeadingPtPlot.legend(loc="upper right")
-ewBosonLeadingPtCanvas.savefig("ewBosonLeadingPtPlot.svg")
+ewBosonLeadingPtCanvas.savefig(argv[0] + "_ewBosonLeadingPtPlot.svg")
 
 ewBosonSubLeadingPtPlot.set_xlabel("pt [GeV]")
 ewBosonSubLeadingPtPlot.set_ylabel("Counts")
 ewBosonSubLeadingPtPlot.set_title("EW Boson sub-leading pt distribution")
 ewBosonSubLeadingPtPlot.grid(True)
 ewBosonSubLeadingPtPlot.legend(loc="upper right")
-ewBosonSubLeadingPtCanvas.savefig("ewBosonSubLeadingPtPlot.svg")
+ewBosonSubLeadingPtCanvas.savefig(argv[0] + "_ewBosonSubLeadingPtPlot.svg")
 
 ewBosonsPtPlot.set_xlabel("pt [GeV]")
 ewBosonsPtPlot.set_ylabel("Counts")
 ewBosonsPtPlot.set_title("EW Bosons pt distribution")
 ewBosonsPtPlot.grid(True)
 ewBosonsPtPlot.legend(loc="upper right")
-ewBosonsPtCanvas.savefig("ewBosonsPtPlot.svg")
+ewBosonsPtCanvas.savefig(argv[0] + "_ewBosonsPtPlot.svg")
 
 chargedLeptonPtPlot.set_xlabel("pt [GeV]")
 chargedLeptonPtPlot.set_ylabel("Counts")
 chargedLeptonPtPlot.set_title("Charged leptons pt distribution")
 chargedLeptonPtPlot.grid(True)
 chargedLeptonPtPlot.legend(loc="upper right")
-chargedLeptonPtCanvas.savefig("chargedLeptonPtPlot.svg")
+chargedLeptonPtCanvas.savefig(argv[0] + "_chargedLeptonPtPlot.svg")
 
 neutralLeptonPtPlot.set_xlabel("pt [GeV]")
 neutralLeptonPtPlot.set_ylabel("Counts")
 neutralLeptonPtPlot.set_title("Neutral leptons pt distribution")
 neutralLeptonPtPlot.grid(True)
 neutralLeptonPtPlot.legend(loc="upper right")
-neutralLeptonPtCanvas.savefig("neutralLeptonPtPlot.svg")
+neutralLeptonPtCanvas.savefig(argv[0] + "_neutralLeptonPtPlot.svg")
 
 leptonsPtPlot.set_xlabel("pt [GeV]")
 leptonsPtPlot.set_ylabel("Counts")
 leptonsPtPlot.set_title("Leptons pt distribution")
 leptonsPtPlot.grid(True)
 leptonsPtPlot.legend(loc="upper right")
-leptonsPtCanvas.savefig("leptonsPtPlot.svg")
+leptonsPtCanvas.savefig(argv[0] + "_leptonsPtPlot.svg")
 
 higgsEtaPlot.set_xlabel("$\eta$")
 higgsEtaPlot.set_ylabel("Counts")
 higgsEtaPlot.set_title("Higgs boson $\eta$ distribution")
 higgsEtaPlot.grid(True)
 higgsEtaPlot.legend(loc="upper right")
-higgsEtaCanvas.savefig("higgsEtaPlot.svg")
+higgsEtaCanvas.savefig(argv[0] + "_higgsEtaPlot.svg")
 
 ewBosonEtaPlot.set_xlabel("$\eta$")
 ewBosonEtaPlot.set_ylabel("Counts")
 ewBosonEtaPlot.set_title("EW bosons $\eta$ distribution")
 ewBosonEtaPlot.grid(True)
 ewBosonEtaPlot.legend(loc="upper right")
-ewBosonEtaCanvas.savefig("ewBosonEtaPlot.svg")
+ewBosonEtaCanvas.savefig(argv[0] + "_ewBosonEtaPlot.svg")
 
 chargedLeptonEtaPlot.set_xlabel("$\eta$")
 chargedLeptonEtaPlot.set_ylabel("Counts")
 chargedLeptonEtaPlot.set_title("Charged leptons $\eta$ distribution")
 chargedLeptonEtaPlot.grid(True)
 chargedLeptonEtaPlot.legend(loc="upper right")
-chargedLeptonEtaCanvas.savefig("chargedLeptonEtaPlot.svg")
+chargedLeptonEtaCanvas.savefig(argv[0] + "_chargedLeptonEtaPlot.svg")
 
 higgsPhiPlot.set_xlabel("$\phi$")
 higgsPhiPlot.set_ylabel("Counts")
 higgsPhiPlot.set_title("Higgs boson $\phi$ distribution")
 higgsPhiPlot.grid(True)
 higgsPhiPlot.legend(loc="upper right")
-higgsPhiCanvas.savefig("higgsPhiPlot.svg")
+higgsPhiCanvas.savefig(argv[0] + "_higgsPhiPlot.svg")
 
 ewBosonPhiPlot.set_xlabel("$\phi$")
 ewBosonPhiPlot.set_ylabel("Counts")
 ewBosonPhiPlot.set_title("EW bosons $\phi$ distribution")
 ewBosonPhiPlot.grid(True)
 ewBosonPhiPlot.legend(loc="upper right")
-ewBosonPhiCanvas.savefig("ewBosonPhiPlot.svg")
+ewBosonPhiCanvas.savefig(argv[0] + "_ewBosonPhiPlot.svg")
 
 chargedLeptonPhiPlot.set_xlabel("$\phi$")
 chargedLeptonPhiPlot.set_ylabel("Counts")
 chargedLeptonPhiPlot.set_title("Charged leptons $\phi$ distribution")
 chargedLeptonPhiPlot.grid(True)
 chargedLeptonPhiPlot.legend(loc="upper right")
-chargedLeptonPhiCanvas.savefig("chargedLeptonPhiPlot.svg")
+chargedLeptonPhiCanvas.savefig(argv[0] + "_chargedLeptonPhiPlot.svg")
