7383fc3c6f822b98e87279d2112e7af1db6d0f9f
Simone Bologna <smnbologna@gmail.com>
2017-04-24 12:52:09 +0100

    findDecayNode now ignores emissions without decay

--- PythiaUtils.h
+++ PythiaUtils.h
@@ -1,3 +1,6 @@
+#ifndef PYTHIAUTILS_H
+#define PYTHIAUTILS_H
+
 #include "Pythia8/Pythia.h"
 
 namespace PythiaUtils{
@@ -9,11 +12,30 @@ namespace PythiaUtils{
   */
   const Pythia8::Particle* findDecayNode(const Pythia8::Event* aEvent, const Pythia8::Particle* aUnstableParticle)
   {
-    const unsigned int numberOfDaughters = aUnstableParticle -> daughterList().size();
-    if (numberOfDaughters > 1) return aUnstableParticle;
-    else return findDecayNode(aEvent, & (*aEvent)[aUnstableParticle -> daughterList()[0]]);
+    const unsigned int lNumberOfDaughters = aUnstableParticle -> daughterList().size();
+    const int lUnstableParticlePGDID = aUnstableParticle -> id();
 
+    if (lNumberOfDaughters > 1) {
+      //Might be a bremsstrahlung event, or any kind of emission. I check if my original particle is still there
+
+      const Pythia8::Particle * lDecayProducts[2];
+      lDecayProducts[0] = & (*aEvent)[aUnstableParticle -> daughterList()[0]];
+      lDecayProducts[1] = & (*aEvent)[aUnstableParticle -> daughterList()[1]];
+      if (lDecayProducts[0] -> id() == lUnstableParticlePGDID) {
+        // We have an emission event, I still have to look for the decay node
+        return findDecayNode(aEvent, lDecayProducts[0]);
+      }
+      if (lDecayProducts[1] -> id() == lUnstableParticlePGDID) {
+        // We have an emission event, I still have to look for the decay node
+        return findDecayNode(aEvent, lDecayProducts[1]);
+      }
+      return aUnstableParticle;
+    }
+    else return findDecayNode(aEvent, & (*aEvent)[aUnstableParticle -> daughterList()[0]]);
+    
     return NULL;
   }
 
 }
+
+#endif //  PYTHIAUTILS_H
