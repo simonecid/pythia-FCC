107d97f687c97f401ae624a4d4fe01c4a0cb81fc
Simone Bologna <smnbologna@gmail.com>
2017-05-03 17:40:07 +0100

    Updated with new cmnd and exe

--- runPythiaWithRoot.sh
+++ runPythiaWithRoot.sh
@@ -2,6 +2,11 @@
 
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.9/x86_64-slc6/setup.sh
 source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.08.06-c8fb4/x86_64-slc6-gcc49-opt/bin/thisroot.sh
-/software/sb17498/pythia8223/pythia-FCC/generateResonanceToHepMCAndROOT.exe -c /software/sb17498/pythia8223/pythia-FCC/ffZee.cmnd -o ffZee_1000evts
-/software/sb17498/pythia8223/pythia-FCC/generateResonanceToHepMCAndROOT.exe -c /software/sb17498/pythia8223/pythia-FCC/ffZmumu.cmnd -o ffZmumu_1000evts
-/software/sb17498/pythia8223/pythia-FCC/generateResonanceToHepMCAndROOT.exe -c /software/sb17498/pythia8223/pythia-FCC/ffZtautau.cmnd -o ffZtautau_1000evts
+/software/sb17498/pythia8223/pythia-FCC/generateHiggs_WWZZ_leptons_HepMC_ROOT.exe -c ff_H_WW_enuenu.cmnd -o ff_H_WW_enuenu
+/software/sb17498/pythia8223/pythia-FCC/generateHiggs_WWZZ_leptons_HepMC_ROOT.exe -c ff_H_WW_munumunu.cmnd -o ff_H_WW_munumunu
+/software/sb17498/pythia8223/pythia-FCC/generateHiggs_WWZZ_leptons_HepMC_ROOT.exe -c ff_H_ZZ_eeee.cmnd -o ff_H_ZZ_eeee
+/software/sb17498/pythia8223/pythia-FCC/generateHiggs_WWZZ_leptons_HepMC_ROOT.exe -c ff_H_ZZ_mumumumu.cmnd -o ff_H_ZZ_mumumumu
+/software/sb17498/pythia8223/pythia-FCC/generateResonanceToTwoParticle_HepMC_ROOT.exe -c ff_W_enu.cmnd -o ff_W_enu_14TeV
+/software/sb17498/pythia8223/pythia-FCC/generateResonanceToTwoParticle_HepMC_ROOT.exe -c ff_W_munu.cmnd -o ff_W_munu_14TeV
+/software/sb17498/pythia8223/pythia-FCC/generateResonanceToTwoParticle_HepMC_ROOT.exe -c ff_Z_ee.cmnd -o ff_Z_ee_14TeV
+/software/sb17498/pythia8223/pythia-FCC/generateResonanceToTwoParticle_HepMC_ROOT.exe -c ff_Z_mumu.cmnd -o ff_Z_mumu_14TeV
