4e3c5a55738fdaa63421638957e979ff8918650e
Simone Bologna <smnbologna@gmail.com>
2018-02-06 16:12:18 +0000

    Prepared for MB production

--- generateEventsToHepMC.cc
+++ generateEventsToHepMC.cc
@@ -13,41 +13,59 @@ int main(int argc, char * argv[]) {
   // Generator. Process selection. LHC initialization. Histogram.
 
   // -c -> command file
-  // -n -> .dat file name suffix for unique output identification
+  // -o -> path of output file
+  // -n -> number of events to generate
 
   std::string cmndFileName;
-  std::string fileSuffix;
+  std::string outputFilename;
+  int numberOfEvents = -1;
+  std::string setSeedCommand = "Random:seed  = ";
+
 
   int c;
-
-  while ((c = getopt (argc, argv, "cn")) != -1)
+  while ((c = getopt (argc, argv, "cnos")) != -1)
   switch (c)
   {
-    case 'c':      
+    case 'c': {
       cmndFileName = argv[optind];
       break;
-    
-    case 'n':
-      fileSuffix = argv[optind];
-      break;
+    }
 
+    case 'o': {
+      outputFilename = argv[optind];
+      break;
+    }
+
+    case 'n': {
+      std::string numberOfEvents_str = argv[optind];
+      std::istringstream iss;
+      iss.str(numberOfEvents_str);
+      iss >> numberOfEvents;
+      break;
+    }
+
+    case 's': {
+      std::string seed = argv[optind];
+      std::cout << "seed has been set to " << seed << std::endl;
+      setSeedCommand.append(seed);
+      break;
+    }
     default:
       abort ();
   }
 
   Pythia8::Pythia pythia;
   HepMC::Pythia8ToHepMC ToHepMC;
-  std::string hepMCFileName = "/software/sb17498/pythia8223/mystuff/minBiasEvents/minBias_";
-  hepMCFileName.append(fileSuffix);
-  hepMCFileName.append(".dat");
-  HepMC::IO_GenEvent ascii_io(hepMCFileName.c_str(), std::ios::out);
+  HepMC::IO_GenEvent ascii_io(outputFilename.c_str(), std::ios::out);
   pythia.readFile(cmndFileName.c_str());
+  pythia.readString("Random:setSeed = on");
+  pythia.readString(setSeedCommand);
   pythia.init();
 
-  int nEvents = pythia.mode("Main:numberOfEvents");
+  if (numberOfEvents <= 0) numberOfEvents = pythia.mode("Main:numberOfEvents");
 
   // Begin event loop. Generate event. Skip if error. List first one.
-  for (int iEvent = 0; iEvent < nEvents; ++iEvent) {
+  for (int iEvent = 0; iEvent < numberOfEvents; ++iEvent) {
     if (!pythia.next()) continue;
 
     HepMC::GenEvent* hepmcevt = new HepMC::GenEvent();
